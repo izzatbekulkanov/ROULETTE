a5f14cdb78b05542e8f536b620cdb859dd598623
Izzatbek Ulkanov <izzatbekulkanov@gmail.com>
2025-05-19 15:17:21 +0500

    a

--- core/settings.py
+++ core/settings.py
@@ -4,7 +4,7 @@ from datetime import timedelta
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
-DEBUG = False
+DEBUG = True
 # Ruxsat etilgan domenlar
 ALLOWED_HOSTS = [
     "localhost",
