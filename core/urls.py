d113ab6e253879b4636528d4a2821aa884e3e3d0
Izzatbek Ulkanov <izzatbekulkanov@gmail.com>
2025-05-19 17:12:26 +0500

    asdqwe

--- core/urls.py
+++ core/urls.py
@@ -11,3 +11,8 @@ urlpatterns = [
 ]
 
 
+# Faqat DEBUG True holatda static va media fayllarni server qilish uchun
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
